9ff1b303281fc5274948f7133f15d945061ccd81
naskapal <co.terbang@gmail.com>
2018-10-21 05:22:26 +0700

    erika tag, added caching feature for setlist

--- services/showService.js
+++ services/showService.js
@@ -6,6 +6,17 @@ const axios = require('axios');
 const FIVE_MINUTES = 1000 * 60 * 5;
 const Promise = require('bluebird');
 const SCRAPER_URL = 'https://asia-northeast1-f4-dev-circle.cloudfunctions.net/jeketiscraper';
+const redis = require('redis')
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+const client = redis.createClient({
+  host: '192.168.0.2'
+});
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 const notifyURL = 'https://bot.kyla.life/v1/line/push';
 
@@ -30,41 +41,58 @@ const getByMember = async (memberName) => {
   return result
 }
 
-const getBySetlist = (setlistName) => {
+const getBySetlist = async setlistName => {
   const NOW = moment().unix();
-  return datastore.queryDatastore('Show', [
-    ['unixTime', '>', NOW],
-    ['showName', '=', setlistName]
-  ])
+  try {
+    let schedules = await getData('Show')
+    let result = []
+    if (schedules) {
+      schedules = JSON.parse(schedules)
+      schedules.data.map(schedule => {
+        if (schedule.unixTime > NOW && schedule.showName === setlistName) {
+          result.push(schedule)
+        }
+      })
+      return result
+    } else {
+      return datastore.queryDatastore('Show', [
+        ['unixTime', '>', NOW],
+        ['showName', '=', setlistName]
+      ])
+    }
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 const schedule = async () => {
   try {
     setInterval(async () => {
       console.log(moment().format('DD-MM-YYYY HH-mm-ss'))
-      const query = datastore.datastore.createQuery('Show').order('unixTime', {
-          descending: true
-        })
-        .limit(1)
+      const query = datastore.datastore.createQuery('Show').order('unixTime', { descending: true }).limit(1)
       const result = await datastore.datastore.runQuery(query)
       const latest = result[0][0].unixTime;
-
+      let changed = false
       const schedules = await axios.get(SCRAPER_URL)
       schedules.data.map(async show => {
-        const unixTime = moment(`${show.date} ${show.showTime}`, 'DD.MM.YYYY HH:mm').unix()
-        show.unixTime = unixTime;
         show.members ? show.members : show.members = null
-        if (unixTime > latest) {
+        if (show.unixTime > latest) {
+          changed = true
           notifySetlistChange(show)
-          datastore.insert('Show', unixTime, show);
+          datastore.insert('Show', show.unixTime, show);
         } else {
-          const pastData = await datastore.getByKey('Show', unixTime)
-          const saveResult = await datastore.insert('Show', unixTime, show);
+          const pastData = await datastore.getByKey('Show', show.unixTime)
+          const saveResult = await datastore.insert('Show', show.unixTime, show);
           if (saveResult[0].indexUpdates > 0) {
+            changed = true
             notifyMemberChange(show, pastData)
           }
         }
       })
+
+      if (changed) {
+        setData('Show', {data: schedules.data})
+      }
     }, FIVE_MINUTES)
   } catch (e) {
     console.error(e)
@@ -94,7 +122,7 @@ const notifyMemberChange = async (showData, pastData) => {
           memberId: result[0][datastore.KEY].id,
           showData: showDetail
         }
-        console.log("updating data")
+        console.log("updating member deletion data")
         console.log(data)
         return await axios.post(notifyURL, data)
       })
@@ -108,7 +136,7 @@ const notifyMemberChange = async (showData, pastData) => {
           memberId: result[0][datastore.KEY].id,
           showData: showDetail
         }
-        console.log("updating data")
+        console.log("updating member add data")
         console.log(data)
         return await axios.post(notifyURL, data)
       })
@@ -132,6 +160,20 @@ const notifySetlistChange = async showData => {
   console.log(obj)
   return await axios.post(notifyURL, obj)
 }
+
+
+const getData = async key => {
+  return await client.getAsync(key)
+}
+
+const setData = async (key, value, timeout) => {
+  if (timeout) {
+    return await client.setexAsync(key, timeout, JSON.stringify(value))
+  } else {
+    return await client.setAsync(key, JSON.stringify(value))
+  }
+}
+
 
 module.exports = {
   findAll,

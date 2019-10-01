be2d5194f26318e1aa2aae67961281c0023a389a
naskapal <co.terbang@gmail.com>
2019-10-02 03:05:01 +0700

    gigacommit for order service

--- services/order.js
+++ services/order.js
@@ -1,21 +1,31 @@
 const {
   transactions: transaction,
   payment_method: paymentMethod,
-  receiving_account: receivingAccount
+  receiving_account: receivingAccount,
+  user_cookie: cookieDB,
+  payment_accounts: paymentAccount
 } = require('../models');
+const Models = require('../models');
 const {
   notIn,
   and,
   lt,
 } = require('sequelize')['Op'];
 const id = require('shortid');
-const MAXIMUM_UNIQUE_PRICE = 200;
-const MINIMUM_UNIQUE_PRICE = 100;
+const MAXIMUM_UNIQUE_PRICE = process.env.MAXIMUM_UNIQUE_PRICE;
+const MINIMUM_UNIQUE_PRICE = process.env.MINIMUM_UNIQUE_PRICE;
 const ONE_MINUTE = 60 * 1000;
 const MAXIMUM_RETRY = 100;
 const moment = require('moment-timezone');
 moment.tz.setDefault('Asia/Jakarta');
 const REUSED_CONDITION = ['EXPIRED', 'COMPLETED'];
+const topup = require('./priceScraper');
+const logger = require('./logger');
+const notify = require('../util/notify');
+const axios = require('axios');
+const {
+  uuidv4
+} = require('../util/uuid');
 
 const cleanFunction = async () => {
   const NOW = moment().format('YYYY-MM-DD HH:mm:ss');
@@ -24,7 +34,7 @@ const cleanFunction = async () => {
     status: 'EXPIRED'
   }, {
     where: {
-      [and] : [{
+      [and]: [{
         expiredAt: {
           [lt]: formattedTime
         }
@@ -41,33 +51,157 @@ const createOrder = async options => {
   if (!options) {
     throw new Error('param cannot be empty');
   } else {
-    if (!options.amount) {
-      return {
-        amounts: await getAvailableAmounts()
-      }
-    }
-    if (!options.paymentMethod) {
-      return {
-        paymentMethods: await getPaymentMethods()
-      }
-    }
+    const e = new Error();
     if (options.confirm) {
       // TODO: if confirmed, fill the payment account id num
+      // logger.log(options);
+      await confirmTransaction(options.trx_id);
+      await waitForPayment(options.trx_id);
+      const {
+        email
+      } = await cookieDB.findByPk(options.lineId);
+      const queryResult = await transaction.findOne({
+        where: {
+          [and]: [{
+              trx_id: options.trx_id
+            },
+            {
+              status: {
+                [notIn]: REUSED_CONDITION
+              }
+            }
+          ]
+        },
+        include: [{
+          model: Models.payment_method,
+          include: [{
+            model: Models.receiving_account
+          }]
+        }]
+      })
+
+      const {
+        trx_id,
+        grand_total,
+        expiredAt
+      } = queryResult;
+      const {
+        name: paymentMethod
+      } = queryResult.payment_method;
+      const {
+        account_number,
+        name
+      } = queryResult.payment_method.receiving_accounts[0];
+
+      const result = {};
+      result.type = 'WAITING_FOR_PAYMENT';
+      result.data = {
+        email,
+        trx_id,
+        grand_total,
+        paymentMethod,
+        account_number,
+        name,
+        expiry: expiredAt
+      }
+      // logger.log(result);
+      return result;
+    } else {
+      if (!options.amount) {
+        e.type = 'NEED_AMOUNT';
+        e.data = await topup.getAvailableAmounts(options.lineId);
+        throw e;
+      }
+      if (!options.paymentMethod) {
+        e.type = 'NEED_PAYMENT_METHOD';
+        e.data = await getPaymentMethods();
+        throw e;
+      }
+
+      if (!options.confirm) {
+        const {
+          pointsConfirmation,
+          profileDetail,
+          trx_id
+        } = await getQuotation(options);
+        e.type = 'WAITING_FOR_CONFIRMATION';
+        e.data = {
+          trx_id,
+          topupConfirmation: pointsConfirmation,
+          profileDetail
+        }
+      }
+      logger.error = e;
+      // logger.log(options);
+      throw e;
     }
-    const quotation = await getQuotation(options);
-    return quotation;
   }
 }
 
+const waitForPayment = async trx_id => {
+  const status = 'WAITING_FOR_PAYMENT';
+  return transaction.update({
+    status
+  }, {
+    where: {
+      [and]: [{
+          trx_id
+        },
+        {
+          status: {
+            [notIn]: REUSED_CONDITION
+          }
+        }
+      ]
+    }
+  });
+}
+
+const confirmTransaction = trx_id => {
+  const status = 'CONFIRMED';
+  return transaction.update({
+    status
+  }, {
+    where: {
+      [and]: [{
+          trx_id
+        },
+        {
+          status: {
+            [notIn]: REUSED_CONDITION
+          }
+        }
+      ]
+    }
+  });
+}
+
 const getQuotation = async options => {
-  const {
+  let {
     paymentMethod: name,
     lineId,
-    email,
-    amount
+    amount: amountPoint
   } = options;
+  const {
+    email
+  } = cookieDB.findByPk(lineId);
 
-  const paymentAccounts = await receivingAccount.findAll({
+  let paymentDetail;
+  let topupLink = '';
+  if (amountPoint) {
+    amount = await topup.verifyPoints(lineId, amountPoint);
+    paymentDetail = await topup.getPaymentDetail(lineId, amount);
+    topupLink = paymentDetail.topupLink;
+  }
+
+
+  const {
+    pointsConfirmation = '',
+      profileDetail = ''
+  } = paymentDetail.confirmation;
+  const admin_fee = pointsConfirmation['Biaya Administrasi'].split(' ')[1].split(',').join('');
+
+  const receivingAccounts = await receivingAccount.findAll({
     include: [{
       model: paymentMethod,
       where: {
@@ -80,12 +214,12 @@ const getQuotation = async options => {
     }]
   });
 
-  const paymentAccountIndex = Math.floor(Math.random() * paymentAccounts.length);
-  const receivingPaymentAccount = paymentAccounts[paymentAccountIndex];
+  const receivingAccountIndex = Math.floor(Math.random() * receivingAccounts.length);
+  const receivingPaymentAccount = receivingAccounts[receivingAccountIndex];
 
 
-  const detailPayment = await getPaymentDetail(amount, receivingPaymentAccount.payment_method.processing_fee);
-  const status = 'WAITING_FOR_PAYMENT';
+  const detailPayment = await getPaymentDetail(amount, admin_fee, receivingPaymentAccount.payment_method.processing_fee);
+  const status = 'WAITING_FOR_CONFIRMATION';
   const trx_id = await getUniqueTransactionId();
 
   const quotation = await transaction.findOne({
@@ -104,20 +238,23 @@ const getQuotation = async options => {
 
   const {
     subtotal,
-    admin_fee,
     total,
     unique_code,
     grand_total,
     processing_fee
   } = detailPayment;
 
-  const expiredAt = moment().add(2, 'minute').format('YYYY-MM-DD HH:mm:ss');
+  const expiredAt = moment().add(60, 'minute').format('YYYY-MM-DD HH:mm:ss');
   const formattedTime = moment.tz(expiredAt, 'Asia/Jakarta').format();
 
+  console.log("====================");
+  console.log(topupLink);
+  console.log("====================");
+
   const updatedData = await quotation.update({
-    payment_method_id: receivingPaymentAccount.payment_method.id,
+    payment_method_id: Number(receivingPaymentAccount.payment_method.id),
     payment_account_id: null,
-    receiving_account_id: receivingPaymentAccount.id,
+    receiving_account_id: Number(receivingPaymentAccount.id),
     lineId,
     email,
     subtotal, // jumlah yang dibeli
@@ -127,28 +264,30 @@ const getQuotation = async options => {
     unique_code, // unique code
     grand_total, // total semua
     status,
+    topup_url: topupLink,
     expiredAt: formattedTime
   })
 
-  console.log("==============")
-  console.log(updatedData)
-  console.log("==============")
-  // return quotation;
+  pointsConfirmation['Kode Unik'] = unique_code;
+  pointsConfirmation['Handling Fee'] = processing_fee;
+  pointsConfirmation['Total Pembayaran'] = grand_total;
+  pointsConfirmation['Cara Pembayaran'] = name;
+  return {
+    pointsConfirmation,
+    profileDetail,
+    trx_id
+  };
 }
 
-const getAvailableAmounts = async _ => {
-
-}
-
-const getPaymentDetail = async (amount, processing_fee, attempt = 0) => {
+const getPaymentDetail = async (amount, admin_fee, processing_fee, attempt = 0) => {
   if (attempt === MAXIMUM_RETRY) {
     throw new Error(`Error while getting payment detail after 100 times`)
   }
-  const subtotal = amount;
-  const admin_fee = 999 //getAdminFee(amount);
-  const total = subtotal + admin_fee;
+  const subtotal = Number(amount);
+  const total = Number(subtotal) + Number(admin_fee);
   const unique_code = Math.floor(Math.random() * (MAXIMUM_UNIQUE_PRICE - MINIMUM_UNIQUE_PRICE + 1)) + MINIMUM_UNIQUE_PRICE;
-  const grand_total = total + processing_fee + unique_code;
+  const grand_total = Number(total) + Number(processing_fee) + Number(unique_code);
+
   const duplicate = await transaction.findOne({
     where: {
       [and]: [{
@@ -163,7 +302,7 @@ const getPaymentDetail = async (amount, processing_fee, attempt = 0) => {
     }
   })
   if (duplicate) {
-    getPaymentDetail(amount, processing_fee)
+    getPaymentDetail(amount, processing_fee, attempt + 1)
   } else {
     return {
       subtotal,
@@ -214,14 +353,175 @@ const sleep = timeout => {
   })
 }
 
+const getPaymentMethods = async () => {
+  const queryResult = await paymentMethod.findAll({
+    where: {
+      status: true
+    }
+  });
+  const paymentMethods = queryResult.map(row => {
+    return row.name
+  })
+  return paymentMethods
+}
+
+const confirmPayment = async paymentDetails => {
+  try {
+    const {
+      amount
+    } = paymentDetails;
+
+    const transactionQuery = await transaction.findOne({
+      where: {
+        [and]: [{
+            grand_total: amount
+          },
+          {
+            status: {
+              [notIn]: REUSED_CONDITION
+            }
+          }
+        ]
+      }
+    });
+
+    await notify({
+      to: transactionQuery.lineId,
+      message: `Pembayaran sebesar Rp ${amount} untuk kode transaksi ${transactionQuery.trx_id} telah diterima. Sesaat lagi kami akan mengirimkan notifikasi status transaksi top up anda`
+    })
+
+    const paymentMethods = await paymentAccount.findAll({
+      where: {
+        active: true
+      }
+    });
+
+    const paymentCardIndex = Math.floor(Math.random() * paymentMethods.length);
+    const paymentCard = paymentMethods[paymentCardIndex];
+    const {
+      no: num,
+      cvv,
+      expiry
+    } = paymentCard;
+    const {
+      topup_url: link
+    } = transactionQuery;
+
+    if (transactionQuery) {
+      await transactionQuery.update({
+        status: 'PAID',
+        payment_account_id: paymentCardIndex
+      })
+    }
+
+    const POLLING_STOP_CONDITION = (response) => {
+      return [
+        'SUCCESS',
+        'FAILED',
+        'WAITING_FOR_OTP'
+      ].indexOf(response.data.status) > -1;
+    }
+
+    const job_id = uuidv4();
+    const service = 'MIDTRANS';
+
+    const paymentRequest = axios.post.bind(axios.post, process.env.TOPUP_SERVICE_URL, {
+      job_id,
+      service,
+      data: {
+        link,
+        num,
+        cvv,
+        expiry
+      }
+    });
+
+    const response = await retryPromise(paymentRequest, POLLING_STOP_CONDITION, 2000);
+    if (response.data.status === 'WAITING_FOR_OTP') {
+      await sleep(20000);
+      const messages = await axios.get(process.env.SMS_SERVICE_URL, {
+        headers: {
+          'x-api-key': process.env.API_KEY
+        }
+      });
+
+      const msgstring = await messages.data.find(e => e.content.includes('BTPN'));
+      const split = msgstring.content.split('is ');
+      const otp = split[1].slice(0, 6);
+
+
+      const submitOtp = axios.post.bind(axios.post, process.env.TOPUP_SERVICE_URL, {
+        job_id,
+        service,
+        data: {
+          link,
+          num,
+          cvv,
+          expiry,
+          otp
+        }
+      }, {
+        headers: {
+          'x-api-key': process.env.API_KEY
+        }
+      });
+
+      const result = await retryPromise(submitOtp, POLLING_STOP_CONDITION, 2000);
+
+      if (result) {
+        await notify({
+          to: transactionQuery.lineId,
+          message: `Transaksi top up dengan kode transaksi ${transactionQuery.trx_id} berhasil. Silakan cek JKT48 points anda melalui perintah /mypage`
+        });
+        await transaction.update({
+          status: 'Completed'
+        }, {
+          where: {
+            [and]: [{
+                trx_id
+              },
+              {
+                status: {
+                  [notIn]: REUSED_CONDITION
+                }
+              }
+            ]
+          }
+        })
+      }
+    }
+
+
+
+  } catch (e) {
+    console.error(e);
+    // send to elasticsearch
+    console.log("error in service, function confirmPayment")
+  }
+}
+
 const cleaner = setInterval(cleanFunction, ONE_MINUTE);
 
-// immediately invoke cleanFunction
+const retryPromise = (fn, stopCondition, retryInterval) => {
+  console.log('retrying....')
+  return fn()
+    .then(data => {
+      if (stopCondition(data)) {
+        return data;
+      }
+
+      return new Promise(resolve => {
+        retryInterval = retryInterval || 1000;
+        setTimeout(function () {
+          resolve(retryPromise(fn, stopCondition, retryInterval));
+        }, retryInterval);
+      });
+    });
+}
+
 (cleanFunction)()
 
-// TODO: ALLOW REUSE COMPLETED TRX ID
-// TODO: ADD TRANSACTION
-
 module.exports = {
-  createOrder
+  createOrder,
+  confirmPayment
 }

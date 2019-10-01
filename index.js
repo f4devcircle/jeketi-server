1244c2453873eb404620c3b7d46a64a64ed18ac4
naskapal <co.terbang@gmail.com>
2019-10-02 02:58:18 +0700

    some beautification

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ require('./services/apm');
 const express = require('express');
 const bodyParser = require('body-parser');
 const shows = require('./routes/shows.route');
+const profile = require('./routes/profile');
 const scrapeSchedule = require('./services/showService');
 const memberScrape = require('./services/membersService');
 const auth = require('./routes/auth');
 const buy = require('./routes/buy');
+const topup = require('./routes/topup');
 const cors = require('cors');
 
 scrapeSchedule.schedule();
-memberScrape.membersScheduler();
+// memberScrape.membersScheduler();
 
 const app = express();
 
@@ -20,22 +22,24 @@ app.use(cors());
 app.use('/shows', shows);
 app.use('/auth', auth);
 app.use('/buy', buy);
+app.use('/profile', profile);
+app.use('/topup', topup);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 })
 
 // error handler
 app.use((err, req, res, next) => {
-  res.locals.message = err.message
-  console.log(err)
-  res.status(err.status || 500)
+  res.locals.message = err.message;
+  console.log(err);
+  res.status(err.status || 500);
   res.send({
     error: err
-  })
+  });
 })
 
 app.listen(process.env.APPLICATION_PORT, () => {
